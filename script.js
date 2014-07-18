460530b054fc21e925fce78bc1dd3baf8120682c
james <jamkcas@yahoo.com>
2014-07-17 23:06:27 -0700

    Drag and Drop code snippet

---
+++ script.js
@@ -0,0 +1,29 @@
+var items = document.getElementsByClassName('listItem');
+
+function dragStart(e) {
+  e.dataTransfer.setData('text/plain', e.target.className);
+}
+
+function dragEnd(e) {
+  e.preventDefault();
+  var data = e.dataTransfer.getData('text/plain');
+
+  if(e.target.className.match(/listItem/)) {
+    var y = e.clientY || e.pageY;
+    if(y < e.target.offsetTop + (e.target.clientHeight / 2)) {
+      e.target.parentNode.insertBefore(document.getElementsByClassName(data)[0], e.target);
+    } else {
+      if(e.target.nextSibling) {
+        e.target.parentNode.insertBefore(document.getElementsByClassName(data)[0], e.target.nextSibling);
+      } else {
+        e.target.parentNode.appendChild(document.getElementsByClassName(data)[0]);
+      }
+    }
+  } else if(e.target.className.match(/itemList/)) {
+    e.target.appendChild(document.getElementsByClassName(data)[0]);
+  }
+}
+
+function dragAllow(e) {
+  e.preventDefault();
+}
